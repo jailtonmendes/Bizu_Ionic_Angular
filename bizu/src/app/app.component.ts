3daaaf1d7b83d09edd8efbe540a335b1db01747f
jailtonmendes <jailtonmendes.silva@gmail.com>
2021-10-27 10:32:58 -0300

    Subindo bizu
    repositório para guardar codigos

---
+++ bizu/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+  img1: any;
+
+  public appPages = [
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+
+  //Preview Foto
+  fileChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+      reader.onload = (event:any) => {
+        this.img1 = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);  // to trigger onload
+    }
+
+    let fileList: FileList = event.target.files;
+    let file: File = fileList[0];
+    console.log(file);
+  }
+
+}
+
